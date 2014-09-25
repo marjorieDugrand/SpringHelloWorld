54cb0fd14bf271bdebfd1a1dbeccd0b1e7a79896
unknown <dugrandm@ELSOXFL-7030716.science.regn.net>
2014-09-25 16:39:10 +0100

    |pages in angular ok|

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ SpringHelloWorld
 Example projects putting together Spring with other technologies
 
 See the different braches for different versions.
+
+I did not have time to control all the spring dependencies and the java classes, I just faked a user list in my javascript controller
+(so the data is lost each time the page is loaded)
+the page can be accessed through localhost:8080/HelloWorld/index.html
--- src/main/webapp/WEB-INF/js/helloWorld.js
+++ src/main/webapp/WEB-INF/js/helloWorld.js
@@ -4,11 +4,11 @@ var countID = 1;
 
 var User = function (username,password,emailAddress,firstName,lastName) {
 	this.id = countID ++;
-	this.username = username;
-	this.password = password;
-	this.emailAddress = emailAddress;
-	this.firstName = firstName;
-	this.lastName = lastName;
+	this.username = username || '';
+	this.password = password || '';
+	this.emailAddress = emailAddress || '';
+	this.firstName = firstName || '';
+	this.lastName = lastName || '';
 };
 
 var users = [
@@ -16,74 +16,57 @@ var users = [
              new User("Segolene", "321", "segolene@gmail.com", "Segolene", "Morris")
 ];
 
-var userEdition = false;
-var isUserNew = true;
-var editingUser = null;
-
-
 var UserService = function ($http, users) {
 	this.$http = $http;
 	this.users = users;
 };
+
 UserService.prototype.loadUsers = function () {
 //	return $http.get("/");
 	return this.users;
 };
 
-UserService.prototype.addUser = function () {
-	userEdition = true;
-	isUserNew = true;
-	editingUser = new User("","","","","");
-};
-
-UserService.prototype.accessUser = function (user) {
-	userEdition = true;
-	isUserNew = false;
-	editingUser = user;
-};
-
-UserService.prototype.loadUser = function() {
-	return editingUser;
-};
-
-
-UserService.prototype.showUserEdition = function() {
-	return userEdition;
-};
-
-UserService.prototype.isNewUser = function() {
-	return isUserNew;
-};
-
-UserService.prototype.saveUser = function() {
-	console.log(editingUser.username);
-	console.log(editingUser.emailAddress);
-	if(UserService.prototype.isNewUser()) {
-		users.push(editingUser);
-	}
-	userEdition = false;
-};
-
-UserService.prototype.deleteUser = function() {
-	var index = users.indexOf(editingUser);
-	users = users.slice(index, 1);
-}
-
-
 helloWorldModule.service("UserService", ["$http", function($http) { return new UserService($http, users); }]);
 
 helloWorldModule.controller("UserController", ["$scope", "UserService", function ($scope, UserService) {
-//	var get = UserService.loadUsers();
-//	get.success(function(users) {
-//		$scope.users = users;
-//	});
+
+	$scope.weAreEditing = false;
+	$scope.creatingNewUser = false;
+	
 	$scope.users = UserService.loadUsers();
-	$scope.addUser = UserService.addUser;
-	$scope.accessUser = UserService.accessUser;
-	$scope.showUserEdition = UserService.showUserEdition;
-	$scope.isNewUser = UserService.isNewUser;
-	$scope.editingUser = UserService.loadUser();
-	$scope.saveUser = UserService.saveUser;
-	$scope.deleteUser = UserService.deleteUser;
+	
+	$scope.addUser = function() {
+		$scope.weAreEditing = true;
+		$scope.creatingNewUser = true;
+		$scope.editingUser = new User();
+	};
+	
+	$scope.accessUser = function (user) {
+		$scope.editingUser = user;
+		$scope.weAreEditing = true;
+		$scope.creatingNewUser = false;
+	};
+	$scope.showUserEdition = function() {
+		return $scope.weAreEditing;
+	};
+	$scope.showNewUserCreation = function() {
+		return $scope.creatingNewUser;
+	};
+	
+
+	$scope.editingUser = new User();
+	$scope.saveUser = function() {
+		if($scope.showNewUserCreation()) {
+			UserService.users.push($scope.editingUser);
+		}
+		$scope.weAreEditing = false;
+	};
+	
+	$scope.deleteUser = function() {
+		var index = $scope.users.indexOf($scope.editingUser);
+		$scope.users.splice(index, 1);
+		$scope.weAreEditing = false;
+	}
+	;
 }]);
 
